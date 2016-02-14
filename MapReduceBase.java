486a6ac55db8713016a325f00b4520cc45c99ae5
Zebin Xie <zebin.xie@gmail.com>
2016-02-14 05:10:52 -0600

    [hw1] Q4 finish, calculate age only by year. Use three steps MapReduce. #hadoop jar hadoop.jar TopAverageAge /zxx140430/input/hw1-Adj /zxx140430/input/hw1-userdata /zxx140430/output/1

--- MapReduceBase.java
+++ MapReduceBase.java
@@ -2,12 +2,22 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 
 /**
  * Created by xiezebin on 2/13/16.
@@ -15,6 +25,87 @@ import org.apache.hadoop.util.GenericOptionsParser;
 public class MapReduceBase
 {
     protected final static String DRIVER_TO_MAPPER_KEY = "DRIVER_TO_MAPPER_KEY";
+
+    // custom Pair, as composite key, used for reduce-side join
+    protected static class TextPair implements WritableComparable<TextPair>
+    {
+        public Text obFirstText;
+        public Text obSecondText;
+
+        TextPair()
+        {
+            obFirstText = new Text();
+            obSecondText = new Text();
+        }
+        TextPair(String arFriendID, String arMutualFriendID)
+        {
+            obFirstText = new Text(arFriendID);
+            obSecondText = new Text(arMutualFriendID);
+        }
+
+        public Text getFirst() {
+            return obFirstText;
+        }
+        public Text getSecond() {
+            return obSecondText;
+        }
+
+        @Override
+        public void write(DataOutput dataOutput) throws IOException {
+            obFirstText.write(dataOutput);
+            obSecondText.write(dataOutput);
+        }
+
+        @Override
+        public void readFields(DataInput dataInput) throws IOException {
+            obFirstText.readFields(dataInput);
+            obSecondText.readFields(dataInput);
+        }
+
+        // compare by first value
+        public int compareTo(TextPair arPair)
+        {
+            int cmp = obFirstText.compareTo(arPair.getFirst());
+            if (cmp != 0)
+            {
+                return cmp;
+            }
+            else
+            {
+                return obSecondText.compareTo(arPair.getSecond());
+            }
+        }
+
+        public String toString()
+        {
+            return obFirstText.toString() + ":" + obSecondText.toString();
+        }
+    }
+
+    // partition and group by first value of TextPair
+    public static class FirstPartitioner extends Partitioner<TextPair, Writable>
+    {
+        @Override
+        public int getPartition(TextPair key, Writable value, int numPartitions)
+        {
+            return Math.abs(key.getFirst().hashCode() * 127) % numPartitions;         // mixing
+        }
+    }
+    public static class GroupComparator extends WritableComparator
+    {
+        protected GroupComparator()
+        {
+            super(TextPair.class, true);
+        }
+        @Override
+        public int compare(WritableComparable w1, WritableComparable w2)
+        {
+            TextPair p1 = (TextPair) w1;
+            TextPair p2 = (TextPair) w2;
+            return p1.getFirst().compareTo(p2.getFirst());
+        }
+    }
+
 
     protected static String[] getOtherArgs(String[] args, int numOfOtherArgs, String arUsage) throws Exception
     {
@@ -27,6 +118,7 @@ public class MapReduceBase
         }
 
         // save target user id
+        /*
         String loTargetUIDs = otherArgs[numOfOtherArgs - 1];
         String[] loTargetUIDsp = loTargetUIDs.split(",");
         if (loTargetUIDsp.length != 2)
@@ -34,7 +126,7 @@ public class MapReduceBase
             System.err.println(arUsage);
             System.exit(2);
         }
-
+        */
         return otherArgs;
     }
 
@@ -48,8 +140,7 @@ public class MapReduceBase
     {
         Configuration conf = new Configuration();
         // set argument
-        String loTargetUIDs = arDriverToMapperVal;
-        conf.set(DRIVER_TO_MAPPER_KEY, loTargetUIDs);
+        conf.set(DRIVER_TO_MAPPER_KEY, arDriverToMapperVal);
 
         // reuse output folder
         FileSystem loFS = FileSystem.get(new Configuration());
@@ -58,7 +149,7 @@ public class MapReduceBase
 
         // create a job with name CLASS_NAME
         Job job = new Job(conf, "JOB");
-        job.setJarByClass(MutualFriend.class);
+        job.setJarByClass(MapReduceBase.class);
         job.setMapperClass(arMapClass);
         job.setReducerClass(arReduceClass);
 
@@ -71,7 +162,45 @@ public class MapReduceBase
         // set the HDFS path for the output
         FileOutputFormat.setOutputPath(job, new Path(outputPath));
 
-        //Wait till job completion
+        return job;
+    }
+
+    protected static Job setupMultiInputJob(
+            Class<? extends Mapper> arMapClass1,
+            Class<? extends Mapper> arMapClass2,
+            Class<? extends Reducer> arReduceClass,
+            String inputPath1,
+            String inputPath2,
+            String outputPath,
+            Class<?> arOutputKeyClass,
+            Class<?> arOutputValueClass,
+            String arDriverToMapperVal)
+            throws Exception
+    {
+        Configuration conf = new Configuration();
+        // set argument
+        conf.set(DRIVER_TO_MAPPER_KEY, arDriverToMapperVal);
+
+        // reuse output folder
+        FileSystem loFS = FileSystem.get(new Configuration());
+        loFS.delete(new Path(outputPath), true);
+
+
+        // create a job
+        Job job = Job.getInstance(conf, "JOB");
+        job.setJarByClass(MapReduceBase.class);
+        job.setReducerClass(arReduceClass);
+
+        // set output key type
+        job.setOutputKeyClass(arOutputKeyClass);
+        // set output value type
+        job.setOutputValueClass(arOutputValueClass);
+        //set the HDFS path of the input data
+        MultipleInputs.addInputPath(job, new Path(inputPath1), TextInputFormat.class, arMapClass1);
+        MultipleInputs.addInputPath(job, new Path(inputPath2), TextInputFormat.class, arMapClass2);
+        // set the HDFS path for the output
+        FileOutputFormat.setOutputPath(job, new Path(outputPath));
+
         return job;
     }
 }
--- TopAverageAge.java
+++ TopAverageAge.java
@@ -1,49 +1,311 @@
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
 
 /**
  * Created by xiezebin on 2/13/16.
  */
 public class TopAverageAge extends MapReduceBase
 {
-    private final static String USAGE = "Usage: TopAverageAge <in_adj> <in_data> <out> <user id,user id>";
-    private final static String TEMP_FOLDER = "/zxx140430/output/temp";
+    private final static String USAGE = "Usage: TopAverageAge <in_adj> <in_data> <out>";
+    private final static String TEMP_FOLDER = "/zxx140430/output/temp/1";
+    private final static String TEMP_FOLDER_ALTER = "/zxx140430/output/temp/2";
 
-    public static class MapForStep2
+    private final static String AHEAD_KEY = "0";
+    private final static String BEHIND_KEY = "1";
+
+    private final static String CURRENT_YEAR = "2016";
+    private final static String AGE_TAG = "AGE_TAG";
+    private final static String ADDRESS_TAG = "ADDRESS_TAG";
+
+    // STEP 1: Join two tables by key of each friend in list
+    private static class JoinFriendMapper
+            extends Mapper<LongWritable, Text, TextPair, TextPair>
+    {
+        public void map(LongWritable key, Text value, Context context
+        ) throws IOException, InterruptedException
+        {
+            String[] loLineOfData = value.toString().split("\\t");
+            if (loLineOfData.length < 2)
+            {
+                return;
+            }
+
+            String loCurrentUID = loLineOfData[0];
+            String[] loFriend = loLineOfData[1].split(",");
+            for (int i = 0; i < loFriend.length; i++)
+            {
+                context.write(new TextPair(loFriend[i], BEHIND_KEY), new TextPair(BEHIND_KEY, loCurrentUID));
+            }
+        }
+    }
+    private static class JoinAgeMapper
+            extends Mapper<LongWritable, Text, TextPair, TextPair>
+    {
+        public void map(LongWritable key, Text value, Context context
+        ) throws IOException, InterruptedException
+        {
+            String[] loLineOfData = value.toString().split(",");
+
+            String loCurrentUID = loLineOfData[0];
+            String loDateOfBirth = loLineOfData[9];
+            String loAddress = loLineOfData[3] + ", " + loLineOfData[4] + ", " + loLineOfData[5];
+            Integer loAge = Integer.valueOf(CURRENT_YEAR) - Integer.valueOf(loDateOfBirth.split("/")[2]);
+
+            context.write(new TextPair(loCurrentUID, AHEAD_KEY),
+                    new TextPair(ADDRESS_TAG, loAddress));
+            context.write(new TextPair(loCurrentUID, AHEAD_KEY),
+                    new TextPair(AGE_TAG, "" + loAge));
+        }
+    }
+    private static class JoinFriendAgeReducer
+            extends Reducer<TextPair, TextPair, Text, TextPair>
+    {
+        public void reduce(TextPair key, Iterable<TextPair> values, Context context
+        ) throws IOException, InterruptedException
+        {
+            String loCurrentUID = key.getFirst().toString();
+            int loCount = 0;
+            String loAge = "0";
+
+            for (TextPair lpPair : values)
+            {
+                String loTag = lpPair.obFirstText.toString();
+                String loValue = lpPair.obSecondText.toString();
+                if (loCount < 2)                                    // ahead_key and behind_key group into one iterator
+                {
+                    if (loTag.equals(AGE_TAG))
+                    {
+                        // save age of current user
+                        loAge = loValue;
+                    }
+                    else if (loTag.equals(ADDRESS_TAG))
+                    {
+                        // pass address of current user for future use
+                        context.write(new Text(loCurrentUID), new TextPair(ADDRESS_TAG, loValue));
+                    }
+                    loCount++;
+                }
+                else
+                {
+                    // loValue is friend id, assign current user's age to each friend
+                    context.write(new Text(loValue), new TextPair(AGE_TAG, loAge));
+                }
+            }
+        }
+    }
+
+    // STEP 2: Calculate average age of friends of each user
+    private static class CalculateAveAgeMapper
             extends Mapper<LongWritable, Text, Text, Text>
     {
+        public void map(LongWritable key, Text value, Context context
+        ) throws IOException, InterruptedException
+        {
+            String[] loLineOfData = value.toString().split("\\t");
+            String loCurrentUID = loLineOfData[0];
+            String loTagAndContent = loLineOfData[1];
 
+            context.write(new Text(loCurrentUID), new Text(loTagAndContent));
+        }
     }
-
-    public static class ReduceForStep2
+    private static class CalculateAveAgeReducer
             extends Reducer<Text, Text, Text, Text>
     {
+        public void reduce(Text key, Iterable<Text> values, Context context
+        ) throws IOException, InterruptedException
+        {
+            String loAddress = "";
+            int loAgeSum = 0;
+            int loCount = 0;
 
+            for (Text loText : values)
+            {
+                String[] loData = loText.toString().split(":");
+                String loTag = loData[0];
+                String loContent = loData[1];
+
+                if (loTag.equals(ADDRESS_TAG))
+                {
+                    loAddress = loContent;
+//                    context.write(key, loText);       //do not emit second line
+                }
+                else
+                {
+                    loAgeSum += Integer.valueOf(loContent);
+                    loCount++;
+                }
+            }
+            double loAveAge = loAgeSum * 1.0 / loCount;
+            String loOutput = String.valueOf(loAveAge) + ":" + loAddress;
+            context.write(key, new Text(loOutput));                     // uid <tab> age:address
+        }
     }
+
+    // STEP 3: Sort by average age
+    private static class SortByAgeMapper
+            extends Mapper<LongWritable, Text, TextPair, Text>
+    {
+        public void map(LongWritable key, Text value, Context context
+        ) throws IOException, InterruptedException
+        {
+            String[] loLineOfData = value.toString().split("\\t");      // uid <tab> age:address
+            String loCurrentUID = loLineOfData[0];
+            String[] loAgeAndAddress = loLineOfData[1].split(":");
+            String loAge = loAgeAndAddress[0];
+            String loAddress = loAgeAndAddress[1];
+
+            // same reducer and group, should define sort by loAge
+            context.write(new TextPair("COMMON_KEY", loAge), new Text(loCurrentUID + ":" + loAddress));
+                                                                        // KEY:age <tab> uid:address
+        }
+    }
+    private static class SortByAgeReducer
+            extends Reducer<TextPair, Text, Text, Text>
+    {
+        int obCount = 0;
+        public void reduce(TextPair key, Iterable<Text> values, Context context
+        ) throws IOException, InterruptedException
+        {
+            if (obCount == 20)
+            {
+                return;
+            }
+
+            String loAge = key.getSecond().toString();                  // different ages don't group to same iterator
+            for (Text lpText : values)
+            {
+                String[] loUserAndAddress = lpText.toString().split(":");
+
+                context.write(new Text(loUserAndAddress[0]), new Text(loUserAndAddress[1] + ", " + loAge));
+
+                obCount++;
+                if (obCount == 20)
+                {
+                    break;
+                }
+            }
+        }
+    }
+    private static class SortByAgeDescending extends WritableComparator
+    {
+        protected SortByAgeDescending()
+        {
+            super(TextPair.class, true);
+        }
+        public int compare(WritableComparable w1, WritableComparable w2)
+        {
+            String loAge1 = ((TextPair) w1).getSecond().toString();
+            String loAge2 = ((TextPair) w2).getSecond().toString();
+            double cmp = Double.valueOf(loAge2) - Double.valueOf(loAge1);
+            return cmp > 0 ? 1 : (cmp < 0 ? -1 : 0);
+        }
+    }
+
+    // STEP 4: Join top 20 sorted result with address table, using in-memory join
+    private static class JoinUserMapper
+            extends Mapper<LongWritable, Text, TextPair, Text>
+    {
+        public void map(LongWritable key, Text value, Context context
+        ) throws IOException, InterruptedException
+        {}
+    }
+    private static class JoinAddressMapper
+            extends Mapper<LongWritable, Text, TextPair, Text>
+    {
+        public void map(LongWritable key, Text value, Context context
+        ) throws IOException, InterruptedException
+        {}
+    }
+    private static class JoinUserAddressReducer
+            extends Reducer<TextPair, Text, Text, Text>
+    {
+        public void reduce(TextPair key, Iterable<Text> values, Context context
+        ) throws IOException, InterruptedException
+        {}
+    }
+
 
     // Driver program
     public static void main(String[] args) throws Exception
     {
-        String[] otherArgs = getOtherArgs(args, 4, USAGE);
+        String[] otherArgs = getOtherArgs(args, 3, USAGE);
 
-        // ==== EXEC first step =====================
-        String loTargetUID = otherArgs[3];
-//        Job loJobStep1 = setupJob(MutualFriend.Map.class, MutualFriend.Reduce.class, otherArgs[0], TEMP_FOLDER, loTargetUID);
+        // ==== STEP 1, using reduce-side join =====================
+        Job loJobStepOne = setupMultiInputJob(
+                JoinFriendMapper.class,
+                JoinAgeMapper.class,
+                JoinFriendAgeReducer.class,
+                otherArgs[0],
+                otherArgs[1],
+                TEMP_FOLDER,
+                TextPair.class,
+                TextPair.class,
+                "");
+        loJobStepOne.setPartitionerClass(FirstPartitioner.class);
+        loJobStepOne.setGroupingComparatorClass(GroupComparator.class);
+        boolean loResult = loJobStepOne.waitForCompletion(true);
 
-        // ==== EXEC second step ====================
-//        if (loJobStep1.waitForCompletion(true))
-//        {
-//            Job loJobStep2 = setupJob(MapForStep2.class, ReduceForStep2.class, otherArgs[1], otherArgs[2], TEMP_FOLDER);
-//
-//            System.exit(loJobStep2.waitForCompletion(true) ? 0 : 1);
-//        }
-//        else
-//        {
-//            System.exit(1);
-//        }
+        // ==== STEP 2 =====================
+        if (loResult)
+        {
+            Job loJobStepTwo = setupJob(
+                    CalculateAveAgeMapper.class,
+                    CalculateAveAgeReducer.class,
+                    TEMP_FOLDER,
+                    TEMP_FOLDER_ALTER,
+                    "");
+            loResult = loJobStepTwo.waitForCompletion(true);
+        }
+        else
+        {
+            System.exit(1);
+        }
 
+        // ==== STEP 3 =====================
+        if (loResult)
+        {
+            Job loJobStepThree = setupJob(
+                    SortByAgeMapper.class,
+                    SortByAgeReducer.class,
+                    TEMP_FOLDER_ALTER,
+                    otherArgs[2],
+                    "");
+            loJobStepThree.setOutputKeyClass(TextPair.class);
+            loJobStepThree.setPartitionerClass(FirstPartitioner.class);
+            loJobStepThree.setSortComparatorClass(SortByAgeDescending.class);
+            loResult = loJobStepThree.waitForCompletion(true);
+        }
+        else
+        {
+            System.exit(1);
+        }
+
+        /*
+        // ==== STEP 4 =====================
+        if (loResult)
+        {
+            Job loJobStep2 = setupMultiInputJob(
+                    JoinFriendMapper.class,
+                    JoinAgeMapper.class,
+                    JoinFriendAgeReducer.class,
+                    TEMP_FOLDER,
+                    otherArgs[1],
+                    TEMP_FOLDER_ALTER,
+                    "");
+            loResult = loJobStep2.waitForCompletion(true);
+        }
+        else
+        {
+            System.exit(1);
+        }
+*/
+        System.exit(loResult ? 0 : 1);
     }
 }
